2618d0a3ecab6c58e44de26b4c9ccbc4c6df6086
Ting-Yu Lin <ting-yu.lin@oracle.com>
2022-07-11 18:51:15 -0500

    add triton load test

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 
 # For local testing, specify the model name with the following env variable like ic.
 # export SERVICE_NAME="load-test-pre-trained-ic-vs-model"
-# export MODEL_NAME="ic"
+export MODEL_NAME="ic"
 # export SERVICE_NAMESPACE="test"
 # export NUMBER_OF_USERS=1
 # export SPAWN_RATE=1
-# export RUNNING_TIME=10
+export RUNNING_TIME=3000
 # export IMAGE_VERSION="1.0.0"
 # export PRINCIPAL="user_principal"
 
